94bc1a6a012503c80906c235381636d64a09b918
Tomás Senart <tsenart@gmail.com>
2016-10-29 03:19:30 +0200

    Clean import and function names

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Lib (hexToBase64, xorFixed) where
+module Lib (hexToBase64, xor) where
 
-import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString.Base64.Lazy as B64
-import qualified Data.ByteString.Base16.Lazy as B16
-import Data.Bits (xor)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Base64.Lazy as B64 (encode)
+import qualified Data.ByteString.Base16.Lazy as B16 (encode, decode)
+import qualified Data.Bits as Bits (xor)
 
-hexToBase64 :: B.ByteString -> B.ByteString
+hexToBase64 :: BS.ByteString -> BS.ByteString
 hexToBase64 = B64.encode . fst . B16.decode
 
-xorFixed :: B.ByteString -> B.ByteString -> B.ByteString
-xorFixed p k = B16.encode $ B.pack $ B.zipWith xor (head bs) (last bs)
+xor :: BS.ByteString -> BS.ByteString -> BS.ByteString
+xor p k = B16.encode $ BS.pack $ BS.zipWith Bits.xor (head bs) (last bs)
   where bs = fmap (fst . B16.decode) [p, k]
