f38d4122c4d2755882d119df336d41eb68c2f54d
Tomás Senart <tsenart@gmail.com>
2016-11-05 05:11:54 +0100

    NGrams

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,69 +1,85 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lib (
   toBase16,
   fromBase16,
   hexToBase64,
-  perms,
   similarity,
   xor,
-  xorKeys,
+  keyScores,
   histogram,
-  Histogram(..),
+  Histogram,
   frequencies,
-  kernel,
-  FrequencyTable(..),
-  english,
+  FrequencyTable,
+  Ngram,
+  ngrams,
+  decodeNgramsFromFile
 ) where
 
 import Control.Applicative ((<$>))
-import Control.Monad (replicateM, (>>=), (<=<))
-import Data.Char (ord)
-import Data.Csv
-import Data.List
-import Data.Ord (compare)
-import Data.String (IsString)
-import Data.Word
+import Control.Exception (IOException)
+import Control.Exception.Base (catch)
+import Data.Char (ord, isAscii, isPrint, Char)
+import Data.List (sortBy)
+import Data.Ord (comparing, Down(..))
+import Data.Text.Lazy.Encoding (decodeUtf8', encodeUtf8)
+import GHC.Generics (Generic)
 import Linear
+import Data.Csv
+  ( FromRecord(..)
+  , HasHeader(..)
+  , decode
+  )
+
+import qualified Data.Text.Lazy as T
 import qualified Data.Bits as Bits (xor)
 import qualified Data.ByteString.Base16.Lazy as B16 (encode, decode)
 import qualified Data.ByteString.Base64.Lazy as B64 (encode)
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.Map.Strict as Map
 import qualified Data.Vector as V
 
 -- | Base64 is a base64 encoded byte string
-newtype Base64 = Base64 BS.ByteString
-  deriving (IsString, Eq, Show)
+type Base64 = BS.ByteString
 
 -- | Base16 is a base16 (hex) encoded byte string
-newtype Base16 = Base16 BS.ByteString
-  deriving (IsString, Eq, Show)
+type Base16 = BS.ByteString
 
--- | Histogram is a paramterized map of a given type to Integer counts.
-newtype Histogram k = Histogram (Map.Map k Integer)
-  deriving (Eq, Show)
+-- | Key represents a secret key
+type Key = BS.ByteString
 
--- | FrequencyTable maps k to the percentage it represents in a given alphabet.
-newtype FrequencyTable k = FrequencyTable { fromFreqs :: Map.Map k Double }
-  deriving (Eq, Ord, Show)
+-- | CipherText represents encrypted plain text
+type CipherText = BS.ByteString
 
-type Ngram = BS.ByteString
+-- | PlainText represents decrypted cypher text
+type PlainText = BS.ByteString
+
+-- | Ngram is an Ngram and its number of occurences in a corpus.
+data Ngram = Ngram BS.ByteString Integer deriving (Show, Eq, Ord, Generic)
+
+-- | We make Ngram an instance of CSV.FromNamedRecord in order to load
+-- the Ngram frequency database that is encoded in this format.
+instance FromRecord Ngram
+
+-- | Histogram is a map of Ngrams to their counts.
+type Histogram = Map.Map BS.ByteString Integer
+
+-- | FrequencyTable is a map of Ngrams to their relative frequency in a corpus.
+type FrequencyTable = Map.Map BS.ByteString Double
 
 -- | Returns a decoded BS.ByteString from a Base16 encoded string
 fromBase16 :: Base16 -> BS.ByteString
-fromBase16 (Base16 s) = fst $ B16.decode s
+fromBase16 = fst . B16.decode
 
 -- | Returns a decoded BS.ByteString to a Base16 encoded string
 toBase16 :: BS.ByteString -> Base16
-toBase16 = Base16 . B16.encode
+toBase16 = B16.encode
 
 -- | Returns an encoded Base64 string from a ByteString
 toBase64 :: BS.ByteString -> Base64
-toBase64 = Base64 . B64.encode
+toBase64 = B64.encode
 
 -- | Converts a base16 string to a base64 string
 hexToBase64 :: Base16 -> Base64
@@ -77,61 +93,47 @@ xor a b = BS.pack $ BS.zipWith Bits.xor a' b' where
 
 -- | Returns a histogram of counts of each distinct ngram of length n present
 --   in the given string.
-histogram :: Int -> BS.ByteString -> Histogram Ngram
-histogram 0 _ = Histogram Map.empty
-histogram n s = Histogram $ Map.fromListWith (+) pairs where
-  pairs = zip (ngrams' n s) $ repeat 1
-  ngrams' n ss
-    | (fromIntegral . BS.length) ss < n = []
-    | otherwise = BS.take (fromIntegral n) ss : ngrams' n (BS.drop 1 ss)
+histogram :: [Ngram] -> Histogram
+histogram ns = Map.fromListWith (+) (fmap pairs ns)
+  where pairs (Ngram n c) = (n, c)
+
+-- | Returns a list of Ngrams of size n from the given corpus.
+ngrams :: BS.ByteString -> Int -> [Ngram]
+ngrams _ 0 = []
+ngrams s n
+  | (fromIntegral . BS.length) s < n = []
+  | otherwise = Ngram (BS.take (fromIntegral n) s) 1 : ngrams (BS.drop 1 s) n
 
 -- | Returns a FrequencyTable from the given Histogram.
-frequencies :: Histogram Ngram -> FrequencyTable Ngram
-frequencies (Histogram h) = FrequencyTable $ fmap (truncate 5 . freq) h where
+frequencies :: Histogram -> FrequencyTable
+frequencies h = fmap (digits 5 . freq) h where
   freq x = fromIntegral x / fromIntegral (sum h)
-  truncate n f = fromInteger (round $ f * (10^n)) / (10.0^^n)
+  digits n f = fromInteger (round $ f * (10^n)) / (10.0^^n)
 
--- | Returns the xor keys of limit size sorted by the similarity of the
---   frequency table of the produced plaintext with the given reference.
-xorKeys :: FrequencyTable Ngram -> Int -> BS.ByteString -> [BS.ByteString]
-xorKeys _   0     _      = []
-xorKeys ref limit cipher = sortBy similarity' keys where
-  similarity' a b = similarity ref (freqs b) `compare` similarity ref (freqs a)
-  freqs k = frequencies $ histogram 1 $ xor cipher k
-  keys = perms limit [0..maxBound::Word8]
-
--- | Returns all possible byte strings of the given length with the given
---   alphabet.
-perms :: Int -> [Word8] -> [BS.ByteString]
-perms limit alphabet = BS.pack <$> replicateM limit (sort alphabet)
+-- | Returns a sorted list of tuples of single charachter xor keys and
+-- similarity scores to the reference frequency table.
+keyScores :: FrequencyTable -> CipherText -> [(Key, Double)]
+keyScores ref cipher = sortBy cmp [(k, score k) | k <- keys] where
+  cmp  = comparing $ Down . snd
+  keys = (BS.singleton . fromIntegral . ord) <$>
+    filter pred' [(minBound::Char)..(maxBound::Char)]
+  pred' c = isAscii c && isPrint c
+  score = (similarity ref) . frequencies . histogram . ngrams'
+  ngrams' k = [1..5] >>= (ngrams $ upper $ xor cipher k)
+  upper s = case decodeUtf8' s of
+    Left  _  -> ""
+    Right ss -> encodeUtf8 $ T.toUpper ss
 
 -- | Returns the similarity score of two frequency tables ranging from 0 to 1,
---   where 0 means completely different and 1 means identical.
-similarity :: FrequencyTable Ngram -> FrequencyTable Ngram -> Double
-similarity (FrequencyTable a) (FrequencyTable b) = dot a b / (norm a * norm b)
+-- where 0 means completely different and 1 means identical. Implements the
+-- cosine similarity formula.
+similarity :: FrequencyTable -> FrequencyTable -> Double
+similarity a b = dot a b / (norm a * norm b)
 
--- | English is the English language Ngram frequency table.
-english :: FrequencyTable Ngram
-english = FrequencyTable $ Map.fromList $ zip chars freqs where
-  chars = C.singleton <$> ['A' .. 'Z']
-  freqs = cycle [
-    0.0804, 0.0148, 0.0334, 0.0382, 0.1249, 0.0240, 0.0187, 0.0505,
-    0.0757, 0.0016, 0.0054, 0.0407, 0.0251, 0.0723, 0.0764, 0.0214,
-    0.0012, 0.0628, 0.0651, 0.0928, 0.0273, 0.0105, 0.0168, 0.0023,
-    0.0166, 0.0009]
-
--- | Returns a kernel string for the given frequency table
-kernel :: FrequencyTable Ngram -> BS.ByteString
-kernel (FrequencyTable f) = BS.concat $ Map.foldrWithKey cons [] f where
-  cons ngram freq xs = xs ++ replicate (round $ freq * 10000) ngram
-
--- | Loads the frequency table from the given file.
-{-readFrequencies :: String -> IO (Either String (Histogram Ngram))-}
-{-readFrequencies filename = do-}
-  {-contents <- BS.readFile filename-}
-  {-case decode HasHeader contents of-}
-    {-Left err -> return err-}
-    {-Right v -> V.forM_ v $ \(ngram, count :: Integer) ->-}
-      {-print $ ngram ++ ": " ++ count-}
-
-  {-return Right (Histogram Map.empty)-}
+-- | Reads and decodes Ngrams from a given file.
+decodeNgramsFromFile :: FilePath -> IO (Either String (V.Vector Ngram))
+decodeNgramsFromFile fp = try' (BS.readFile fp) >>= handle where
+  handle = return . either Left (decode NoHeader)
+  try' a = fmap Right a `catch` err
+  err :: IOException -> IO (Either String a)
+  err = return . Left . show
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,14 +3,15 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Data.Map as Map
-import Data.Char (ord)
+import qualified Data.Vector as V
 
 import Lib
 
-tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests :: FrequencyTable -> TestTree
+tests english = testGroup "Tests" [unitTests english]
 
-unitTests = testGroup "Unit tests" [
+unitTests :: FrequencyTable -> TestTree
+unitTests english = testGroup "Unit tests" [
   testCase "https://cryptopals.com/sets/1/challenges/1 (hexToBase64)" $
     hexToBase64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     @?= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
@@ -21,29 +22,15 @@ unitTests = testGroup "Unit tests" [
     @?= fromBase16 "746865206b696420646f6e277420706c6179",
 
   testCase "https://cryptopals.com/sets/1/challenges/3 (histogram)" $
-    histogram 1 "foobar"
-    @?= Histogram (Map.fromList [("a",1),("b",1),("f",1),("o",2),("r",1)]),
+    histogram (ngrams "foobar" 1)
+    @?= Map.fromList [("a",1),("b",1),("f",1),("o",2),("r",1)],
 
   testCase "https://cryptopals.com/sets/1/challenges/3 (frequencies)" $
-    (frequencies . histogram 1) "foobar"
-    @?= FrequencyTable (Map.fromList [
-      ("a",0.16667),("b",0.16667),("f",0.16667),("o",0.33333),("r",0.16667)]),
+    (frequencies . histogram . flip ngrams 1) "foobar"
+    @?= Map.fromList [
+      ("a",0.16667),("b",0.16667),("f",0.16667),("o",0.33333),("r",0.16667)],
 
-  testCase "https://cryptopals.com/sets/1/challenges/3 (perms)" $
-    perms 2 (fromIntegral . ord <$> ['a', 'b'])
-    @?= ["aa", "ab", "ba", "bb"],
-
-  testCase "https://cryptopals.com/sets/1/challenges/3 (similarity, kernel)" $
-    similarity english (frequencies $ histogram 1 $ kernel english)
-    @?= 1,
-
-  testCase "https://cryptopals.com/sets/1/challenges/3 (xorKeys/0)" $
-    xorKeys english 0 "foo"
-    @?= [],
-
-  testCase "https://cryptopals.com/sets/1/challenges/3 (xorKeys/1)" $
-    head (xorKeys english 1 (fromBase16 "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))
-    @?= "X",
+  keyScoresTest english,
 
   testCase "https://cryptopals.com/sets/1/challenges/4 (xor/cycle)" $
     xor "ICE" "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
@@ -51,5 +38,16 @@ unitTests = testGroup "Unit tests" [
 
   ]
 
+keyScoresTest :: FrequencyTable -> TestTree
+keyScoresTest ref = testCase "https://cryptopals.com/sets/1/challenges/3 (crackXor)" $
+  ks @?= [] where
+    ks = xorKeys ref cipher
+    cipher = fromBase16 "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+
 main :: IO ()
-main = defaultMain tests
+main = do
+  ns <- decodeNgramsFromFile "ngrams.csv"
+  case ns of
+    Left err -> putStrLn err
+    Right v -> defaultMain $ tests $ frequencies $ histogram $ V.toList v
